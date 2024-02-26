f0194bff8a939072f7be092330cecf16270dce70
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2024-02-26 22:41:06 +0300

    Update realloc.c
    second commit

--- realloc.c
+++ realloc.c
@@ -1,11 +1,12 @@
 #include "shell.h"
 
 /**
- **_memset - fills memory with a constant byte
- *@s: the pointer to the memory area
- *@b: the byte to fill *s with
- *@n: the amount of bytes to be filled
- *Return: (s) a pointer to the memory area s
+ * _memset - Fills memory with a constant byte.
+ * @s: Pointer to the memory area.
+ * @b: The byte to fill *s with.
+ * @n: The number of bytes to be filled.
+ *
+ * Return: A pointer to the memory area s.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
@@ -17,8 +18,8 @@ char *_memset(char *s, char b, unsigned int n)
 }
 
 /**
- * ffree - frees a string of strings
- * @pp: string of strings
+ * ffree - Frees a string of strings.
+ * @pp: String of strings.
  */
 void ffree(char **pp)
 {
@@ -32,12 +33,12 @@ void ffree(char **pp)
 }
 
 /**
- * _realloc - reallocates a block of memory
- * @ptr: pointer to previous malloc'ated block
- * @old_size: byte size of previous block
- * @new_size: byte size of new block
+ * _realloc - Reallocates a block of memory.
+ * @ptr: Pointer to previous malloc'ated block.
+ * @old_size: Byte size of previous block.
+ * @new_size: Byte size of new block.
  *
- * Return: pointer to da ol'block nameen.
+ * Return: Pointer to the reallocated memory block.
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
@@ -46,7 +47,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (!ptr)
 		return (malloc(new_size));
 	if (!new_size)
-		return (free(ptr), NULL);
+	{
+		free(ptr);
+		return (NULL);
+	}
 	if (new_size == old_size)
 		return (ptr);
 
